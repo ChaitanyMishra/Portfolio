650284da6c8647c3fc0c5c967645757ee5533b28
Chaitanya Mishra <mr.chetan105@gmail.com>
2025-09-05 22:59:03 +0530

    Set base path for GitHub Pages

--- personal-portfolio/vite.config.js
+++ personal-portfolio/vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// ESM fix: define __dirname manually
 const __dirname = new URL('.', import.meta.url).pathname
 
 export default defineConfig({
-  base: '/Portfolio/personal-portfolio/', // ✅ Add this line
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  base: 'Portfolio/personal-portfolio/', // <-- add this line
 })
