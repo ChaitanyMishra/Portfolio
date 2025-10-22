e0b7bfa2b6525a7ad905b8787a4c335f8f803c22
Chaitanya Mishra <mr.chetan105@gmail.com>
2025-10-23 01:00:25 +0530

    fix issue

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,7 @@
-import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+  // Filter out falsy values and join classes
+  const classes = inputs.filter(Boolean).join(' ');
+  return twMerge(classes);
 }
